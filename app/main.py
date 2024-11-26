ae2aa2ada1d3a78d3941159fdb615c0cb6f35194
Monsangter <sangdobae@icloud.com>
2024-11-27 03:21:04 +0900

    [any message]

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ def create_response(request):
     body = struct.pack(">h", error_code)  # error_code: 2 bytes
     body += struct.pack(">B", number_api_keys) #api_version count
     for api in api_versions:
-        body += struct.pack(">hhhh", api["api_key"], api["min_version"], api["max_version"])
+        body += struct.pack(">hhh", api["api_key"], api["min_version"], api["max_version"])
     body += struct.pack(">h", tag_buffer)
     body += struct.pack(">i", throttle_time_ms)
 
