9f3beef634344c1a645273cba1b9199abeaed507
Monsangter <sangdobae@icloud.com>
2024-11-28 03:04:29 +0900

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import socket  # noqa: F401
 import struct
+import asyncio
 
 def create_response(request):
     message_size = struct.unpack(">i", request[:4])[0]
@@ -29,9 +30,41 @@ def create_response(request):
     print(f"Response (Hex): {response.hex()}")
     return response
 
+# def handle_client(conn, addr):
+#     print(f"Connected to {addr}")
+
+#     try:
+#         server = await asyncio.start_server(handle_client, "localhost", 9092)
+    
 
 
-def main():
+#         while True:
+#             request = conn.recv(1024)
+            
+#             response = create_response(request)
+
+#             conn.sendall(response)
+#     except Exception as e:
+#         print(f"{e}")
+#     finally:
+#         conn.close()
+
+async def handle_client(reader, writer):
+    message_size_data = await reader.readexactly(4)
+    message_size = struct.unpack(">i", message_size_data)[0]
+
+    request = await reader.readexactly(message_size)
+
+    response = create_response(request)
+    
+    writer.write(response)
+    await writer.drain()
+
+    writer.close()
+    await writer.wait_closed() 
+
+
+async def main():
     # You can use print statements as follows for debugging,
     # they'll be visible when running tests.
     print("Logs from your program will appear here!")
@@ -41,18 +74,19 @@ def main():
     
     
     try:
-        server = socket.create_server(("localhost", 9092), reuse_port=True)
-    
-        conn, addr = server.accept() # wait for client
+        server = await asyncio.start_server(handle_client, "localhost", 9092)
+        # server = socket.create_server(("localhost", 9092), reuse_port=True)
+        # addr = server.sockets[0].getsockname()    
+        async with server:
+            await server.serve_forever()
 
-        
-        while True:
+        # while True:
+        #     conn, addr = server.accept() # wait for client
+        #     request = conn.recv(1024)
             
-            request = conn.recv(1024)
-            
-            response = create_response(request)
+        #     response = create_response(request)
 
-            conn.sendall(response)
+        #     conn.sendall(response)
     except Exception as e:
         print(f"{e}")
     finally:
