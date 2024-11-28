0de38082a9f0c76b63a3cc2bbb25c8ea0e42a474
Monsangter <sangdobae@icloud.com>
2024-11-28 15:49:55 +0900

    [any message]

--- app/main.py
+++ app/main.py
@@ -42,41 +42,41 @@ def create_response(request):
     print(f"Response (Hex): {response.hex()}")
     return response
 
-def handle_client(conn, addr):
-    print(f"Connected to {addr}")
+# def handle_client(conn, addr):
+#     print(f"Connected to {addr}")
 
-    try:
-        while True:
-            request = conn.recv(1024)
-            
-            response = create_response(request)
-
-            conn.sendall(response)
-    except Exception as e:
-        print(f"{e}")
-    finally:
-        conn.close()
-
-# async def handle_client(reader, writer):
 #     try:
-#         message_size_data = await reader.readexactly(4)
-#         message_size = struct.unpack(">i", message_size_data)[0]
-#         request = await reader.readexactly(message_size)
-        
-#         response = create_response(request)
-#         writer.write(response)
-#         await writer.drain()
-        
-#     except asyncio.IncompleteReadError:
-#         print(f"Connection closed unexpectedly")
+#         while True:
+#             request = conn.recv(1024)
+            
+#             response = create_response(request)
+
+#             conn.sendall(response)
 #     except Exception as e:
-#         print(f"Error: {e}")
+#         print(f"{e}")
 #     finally:
-#         writer.close()
-#         await writer.wait_closed()
+#         conn.close()
+
+async def handle_client(reader, writer):
+    try:
+        message_size_data = await reader.readexactly(4)
+        message_size = struct.unpack(">i", message_size_data)[0]
+        request = await reader.readexactly(message_size)
+        
+        response = create_response(request)
+        writer.write(response)
+        await writer.drain()
+        
+    except asyncio.IncompleteReadError:
+        print(f"Connection closed unexpectedly")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        writer.close()
+        await writer.wait_closed()
 
 
-def main():
+async def main():
     # You can use print statements as follows for debugging,
     # they'll be visible when running tests.
     print("Logs from your program will appear here!")
@@ -85,20 +85,20 @@ def main():
     #
     
     
-    # server = await asyncio.start_server(handle_client, "localhost", 9092)
-    server = socket.create_server(("localhost", 9092), reuse_port=True)
-    # addr = server.sockets[0].getsockname()    
-    # async with server:
-    #     await server.serve_forever()
+    server = await asyncio.start_server(handle_client, "localhost", 9092)
+    # server = socket.create_server(("localhost", 9092), reuse_port=True)
+    addr = server.sockets[0].getsockname()    
+    async with server:
+        await server.serve_forever()
 
-    while True:
-        conn, addr = server.accept() # wait for client
+    # while True:
+    #     conn, addr = server.accept() # wait for client
 
-        client_thread = threading.Thread(target=handle_client, args=(conn, addr))
-        client_thread.start()
+    #     client_thread = threading.Thread(target=handle_client, args=(conn, addr))
+    #     client_thread.start()
 
 
 
 if __name__ == "__main__":
-    main()
-    # asyncio.run(main())
+    # main()
+    asyncio.run(main())
