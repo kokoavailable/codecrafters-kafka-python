a855ba5341ee6b5b44b453b079f6ab723994c87b
Monsangter <sangdobae@icloud.com>
2024-11-28 15:48:34 +0900

    [any message]

--- app/main.py
+++ app/main.py
@@ -32,7 +32,7 @@ def create_response(request):
     body += struct.pack(">i", throttle_time_ms)
     body += struct.pack(">B", 0)
 
-    response_message_size = len(body) + 3
+    response_message_size = len(body) + 4
     header = struct.pack(">i", response_message_size)
     header += struct.pack(">i", correlation_id)
     print(header, response_message_size, correlation_id)
