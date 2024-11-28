c374a116d0792b4822dc17d3e30f70b9d0cb5548
Monsangter <sangdobae@icloud.com>
2024-11-28 21:22:12 +0900

    [any message]

--- app/main.py
+++ app/main.py
@@ -3,34 +3,81 @@ import struct
 import asyncio
 import threading
 
+def parse_describetopic_request(request):
+    length = struct.unpack(">h", request[8:10])[0]
+    offset = 10 + length
+    contents = request[10:offset].decode("utf-8")
+    # buffer = request[offset:offset+1]
+    array_length = request[offset+1:offset+2]
+    topic_name_length = request[offset+2:offset+3]
+    topic_start = offset + 3
+    offset = topic_start + topic_name_length - 1
+    topic_name = request[topic_start:offset]
+    # buffer = request[offset:offset+1]
+    partition_limit = request[offset+1:offset+5]
+    cursor = request[offset+5:offset+6]
+
+    return array_length, topic_name_length, topic_name
+
 def create_response(request):
     print(f"{request.hex()}")
     api_key = struct.unpack(">h", request[:2])[0] # int16
     api_version = struct.unpack(">h", request[2:4])[0] # int16
     correlation_id = struct.unpack(">i", request[4:8])[0] # int32
 
+
+    if api_key == 75:
+        array_length, topic_name_length, topic_name = parse_describetopic_request(request)
+        throttle_time_ms = 0   
+        error_code = 3
+        uuid = "00000000-0000-0000-0000-000000000000"
+        is_internal = 0
+        partitions_array = 0
+        topic_authorized_operations = 3576
+        cursor = 255 # null
+
+        body = struct.pack(">B", 0)
+        body += struct.pack(">i", throttle_time_ms)
+        body += struct.pack(">B", array_length + 1)
+        body += struct.pack(">h", error_code)
+        body += struct.pack(">B", topic_name_length)
+        body += topic_name.encode("utf-8") # contents
+        body += bytes.fromhex(uuid.replace("-", "")) # topic id
+        body += struct.pack(">B", is_internal)
+        body += struct.pack(">B", topic_name_length)
+        body += struct.pack(">B", partitions_array + 1)
+        body += struct.pack(">i", topic_authorized_operations)
+        body = struct.pack(">B", 0)
+        body = struct.pack(">B", cursor)
+        body = struct.pack(">B", 0)
+
+
+
+
+
     # body
-    error_code = 0 if api_version in [0, 1, 2, 3, 4] else 35
-    
-    api_keys = {18:[0, 4],
-                75:[0, 0]}
-    
-    # api_key = 18
-    # min_version, max_version = 0, 4
-    throttle_time_ms = 0
-    tag_buffer = b"\x00"
-    body = struct.pack(">h", error_code)  # error_code: 2 bytes
-    number_api_key = len(api_keys) + 1
-    print(number_api_key)
-    body += struct.pack(">B", number_api_key) #api_version count
-    
-    
-    
-    for key, (min_version, max_version) in api_keys.items():
-        body += struct.pack(">hhh", key, min_version, max_version)
+    if api_key == 18:
+        error_code = 0 if api_version in [0, 1, 2, 3, 4] else 35
+        
+        api_keys = {18:[0, 4],
+                    75:[0, 0]}
+        
+        # api_key = 18
+        # min_version, max_version = 0, 4
+        throttle_time_ms = 0
+        tag_buffer = b"\x00"
+        body = struct.pack(">h", error_code)  # error_code: 2 bytes
+        number_api_key = len(api_keys) + 1
+        print(number_api_key)
+        body += struct.pack(">B", number_api_key) #api_version count
+        
+        
+        
+        for key, (min_version, max_version) in api_keys.items():
+            body += struct.pack(">hhh", key, min_version, max_version)
+            body += struct.pack(">B", 0)
+        body += struct.pack(">i", throttle_time_ms)
         body += struct.pack(">B", 0)
-    body += struct.pack(">i", throttle_time_ms)
-    body += struct.pack(">B", 0)
 
     response_message_size = len(body) + 4
     header = struct.pack(">i", response_message_size)
