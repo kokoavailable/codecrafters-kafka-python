395bc20f98cc1459389227d7205f7eb0d75b9dc3
Monsangter <sangdobae@icloud.com>
2024-11-28 15:15:48 +0900

    [any message]

--- app/main.py
+++ app/main.py
@@ -25,6 +25,7 @@ def create_response(request):
     for key, (min_version, max_version) in api_keys.items():
         body += struct.pack(">B", 0)
         body += struct.pack(">hhh", key, min_version, max_version)
+        body += struct.pack(">B", 0)
     body += struct.pack(">B", 0)
     body += struct.pack(">i", throttle_time_ms)
     body += struct.pack(">B", 0)
